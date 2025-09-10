b987622bd9eddc86643e273e99f9576fd668da0c
Donuts-procodes <donuts.program1010@gmail.com>
2025-09-10 17:51:58 +0530

    minor issues fixed in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/dashboard/',      // <-- Use your repo name here
   plugins: [react()],
 })
